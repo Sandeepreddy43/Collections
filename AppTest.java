40c47a53a799db5722d737167f425facc0fdfaf0
SandeepReddy43 <nccpnm2018@gmail.com>
2019-04-05 21:03:33 +0530

    Bus_greenbus

---
+++ AppTest.java
@@ -0,0 +1,11 @@
+package com.reg.collections.bus;
+
+public class AppTest {
+
+	public static void main(String[] args) {
+//		Customer c = new Customer();
+//		c.askCustomer();
+		Booking b=new Booking();
+		b.showDetails();
+	}
+}
